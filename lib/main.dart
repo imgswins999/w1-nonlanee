636e58ea1ef8017f5a52aae87a5a88b6f63c0fc7
Nonlanee <imgswins999@gmaill.com>
2025-11-24 10:11:24 +0700

    "Second Page 90%"

--- lib/main.dart
+++ lib/main.dart
@@ -244,9 +244,93 @@ class SecondPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(children: [Container(child: Row(children: [
-          Image.network('')
-        ],))]),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  padding:EdgeInsets.only(left: 20) ,
+                  child: ClipOval(
+                    child: Image.network(
+                      'https://play-lh.googleusercontent.com/fk4QLzT0b74DF0LHKUcWcvmNt-zPvsJt_EzMygASLOwqQKUiMq6E2EHzlQ6IGl1kTUuteX4VnzGmxS0_fjLc=w240-h480-rw',
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  child: Column(children: [Text('5',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),), Text('กำลังติดตาม')]),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  child: Column(children: [Text('5M',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),), Text('ผู้ติดตาม')]),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  child: Column(
+                    children: [Text('230K',style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),), Text('ถูกใจและบันทึก')],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text('Nonlanee Uppakhut',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              child: Row(
+                children: [Text('Nonlanee',style: TextStyle(fontSize: 16),), Icon(Icons.arrow_drop_down)],
+              ),
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(left: 20),
+                    width: 350,
+                    child: ElevatedButton(
+                      onPressed: () => Navigator.pushNamed(context, '/second'),
+                      child: Text('คิดตาม'),
+                      style: ElevatedButton.styleFrom(
+
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  Icon(Icons.share),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwEEQluamlCPZqjlN3I7szbB3iTaXIK7lFrA&s',
+                    width: 200,
+                    height: 200,
+                  ),
+                  SizedBox(width: 10,),
+                  Image.network(
+                    'https://preview.redd.it/tung-tung-tung-sahur-scarab-v0-g1jafwgnupve1.jpeg?auto=webp&s=c93809c4fecf3fd4026d7933fd7767c966104825',
+                    width: 200,
+                    height: 200,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
